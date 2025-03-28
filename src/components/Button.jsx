db389477162d6dd09ca8be39162e34899f506f8f
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-03-28 16:08:49 +0100

    lightly adjust Button component for improved code readability

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Button({ language, isSelected, onClick }) {
     return (
         <button type="button"
-            className={`btn btn-lg px-3 py-2 mx-2 my-3 ${isSelected ? "btn-primary text-white" : "btn-outline-primary"
-                }`}
+            className={
+                `btn btn-lg px-3 py-2 mx-2 my-3 ${isSelected ? "btn-primary text-white" : "btn-outline-primary"}`
+            }
             onClick={onClick}
         >
             {language.name}
